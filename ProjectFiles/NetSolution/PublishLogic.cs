af61e7a7399fc1dacd25d57a35e6941b7429c5df
joseoscarcc <joseoscarcc@gmail.com>
2023-10-19 11:34:53 -0600

    PublishLogic created

---
+++ ProjectFiles/NetSolution/PublishLogic.cs
@@ -0,0 +1,27 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.CoreBase;
+using FTOptix.Retentivity;
+using FTOptix.DataLogger;
+using FTOptix.Store;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.HMIProject;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+#endregion
+
+public class PublishLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
